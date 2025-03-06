984d3818a55ad3f1ee6284d591e1aa054ac1f8b4
Thurlerr <andrew_thurler@hotmail.com>
2025-03-06 10:25:24 -0300

    corrigir wincondition

--- tictactoe/scripts.js
+++ tictactoe/scripts.js
@@ -1,3 +1,12 @@
+/////////////////////////////////// T A S K S ////////////////////////////////////////
+//board: criar array 3x3 *******ok******
+//arrumar um jeito de passar o board criado pela creategameboard para dentro da wincondition (insight na linha de wincondition) *******ok******
+//imprimir o quadro no console *******ok******
+//criar lógica de win do jogo  *******ok******
+//organizar melhor linhas 15 a 22 que guardam informações de jogadores *******ok******
+//criar lógica de inserção de valor e impressão no console
+//RESOLVER PROBLEMA DE ATUALIZAÇÃO DA BOARD 
+
 let player1 = ""
 let player2 = ""
 
@@ -6,68 +15,57 @@ const players = {
     nomeP1: player1,
     nomeP2: player2
 }
+
+let mainGameBoard = createGameBoard()
+
 function createGameBoard(){
     const gameBoard = []
 
     for (let i = 0; i < 3; i++){
         const row = []
         for (let j = 0; j < 3; j++){
-            row.push(null)//preencher esse cara com cell, porque mais tarde cell recebe um X ou O,
+            row.push(null)
         }
         gameBoard.push(row)
     }
-
     return gameBoard
 }
 
-function Cell(token){ //refatorar pra ficar menor
+function Cell(token){ //necessario?
     return token
 }
 
-let mainGameBoard = createGameBoard()
-console.log(mainGameBoard)
 function updateGameBoard (row, col, token) {
-    return mainGameBoard[row][col] = token
+    mainGameBoard[row][col] = token
 }
-
-updateGameBoard(0, 1, 'X')
-updateGameBoard(1, 1, 'O')
-console.log(displayBoard(mainGameBoard))
 
 function displayBoard (board){
-   return board.map((row) => row.join("   | ")).join("\n-------------\n")
-}
-
-
-/////////////////////////////////// T A S K S ////////////////////////////////////////
-//board: criar array 3x3 *******ok******
-//arrumar um jeito de passar o board criado pela creategameboard para dentro da wincondition (insight na linha de wincondition) *******ok******
-//imprimir o quadro no console *******ok******
-//criar lógica de win do jogo  *******ok******
-//organizar melhor linhas 15 a 22 que guardam informações de jogadores *******ok******
-//criar lógica de inserção de valor e impressão no console
-
+    return console.log(board.map((row) => row.join("   | ")).join("\n-------------\n"))
+ }
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
 //winCondition precisa ser disparada para cada inserção de valor, para verificar se uma vitória foi atingida
-//winCondition precisa verificar se as posições tem o mesmo valor:
 
-function winCondition (){ //precisa receber o board já populado
-    const board = ['X', 'O', 'O', 'X', 'O', 'O', 'X']
+function winCondition (gameBoard){ //precisa receber o board já populado
+    const board = gameBoard
     const winPattern = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontais
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], //verticais
-    [0, 4, 8], [2, 4, 6] //diagonais
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontais
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], //verticais
+        [0, 4, 8], [2, 4, 6] //diagonais
     ]
     //então, pedir para um if verificar que, se o array retornar true, encerrar o jogo e declarar um vencedor
-
     if (winPattern.some(pattern => pattern.every(index => board[index] === "X"))) return "X venceu!"; //some checa se há algum dos padrões preenchidos, se sim, checa se há apenas X
     if (winPattern.some(pattern => pattern.every(index => board[index] === "O"))) return "O venceu!"; 
-
+    
     return "Nenhum vencedor ainda!"
-        
 }
 
+function playGame (player, row, col, token){
+    updateGameBoard (row, col, token)
+    displayBoard(mainGameBoard)
+    console.log(winCondition(mainGameBoard))
+}
 
+playGame('teste', 1,0,'X')
--- tictactoe/test.js
+++ tictactoe/test.js
@@ -1,3 +1,22 @@
+/////////////////////////////////// T A S K S ////////////////////////////////////////
+//board: criar array 3x3 *******ok******
+//arrumar um jeito de passar o board criado pela creategameboard para dentro da wincondition (insight na linha de wincondition) *******ok******
+//imprimir o quadro no console *******ok******
+//criar lógica de win do jogo  *******ok******
+//organizar melhor linhas 15 a 22 que guardam informações de jogadores *******ok******
+//criar lógica de inserção de valor e impressão no console
+//RESOLVER PROBLEMA DE ATUALIZAÇÃO DA BOARD 
+
+let player1 = ""
+let player2 = ""
+
+player1 = "paulim"
+const players = {
+    nomeP1: player1,
+    nomeP2: player2
+}
+
+let mainGameBoard = createGameBoard()
 
 function createGameBoard(){
     const gameBoard = []
@@ -5,16 +24,47 @@ function createGameBoard(){
     for (let i = 0; i < 3; i++){
         const row = []
         for (let j = 0; j < 3; j++){
-            row.push(Cell())//preencher esse cara com cell, porque mais tarde cell recebe um X ou O,
+            row.push(null)
         }
         gameBoard.push(row)
     }
-
-    return gameBoard.map((row) => row.join("   | ")).join("\n-------------\n")
+    return gameBoard
 }
 
-function Cell(token){ //refatorar pra ficar menor
+function Cell(token){ //necessario?
     return token
 }
 
-console.log(createGameBoard())
+function updateGameBoard (row, col, token) {
+    mainGameBoard[row][col] = token
+}
+
+function displayBoard (board){
+   return console.log(board.map((row) => row.join("   | ")).join("\n-------------\n"))
+}
+
+function winCondition (gameBoard){ //precisa receber o board já populado
+    const board = gameBoard
+    const winPattern = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontais
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], //verticais
+        [0, 4, 8], [2, 4, 6] //diagonais
+    ]
+    if (winPattern.some(pattern => pattern.every(index => board[index] === "X"))) return "X venceu!"; //some checa se há algum dos padrões preenchidos, se sim, checa se há apenas X
+    if (winPattern.some(pattern => pattern.every(index => board[index] === "O"))) return "O venceu!"; 
+    return "Nenhum vencedor ainda!"
+}
+
+function playGame (player, row, col, token){
+    updateGameBoard (row, col, token)
+    displayBoard(mainGameBoard)
+    console.log(winCondition(mainGameBoard))
+}
+
+playGame('teste', 1,0,"X")
+playGame('teste', 1,1,"X")
+playGame('teste', 1,2,"X")
+// console.log(mainGameBoard)
+// updateGameBoard(0,0,'X')
+// console.log(mainGameBoard)
+// displayBoard(mainGameBoard)
